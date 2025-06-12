af969e0239f55419b5b27d324f2aa449d164329c
NgoMinhNhi <ngominhnhi1998@gmail.com>
2025-06-12 10:39:23 +0700

    add script build pm2

---
+++ mt5-agent/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "mt5-agent",
+      script: "mt5_connect.py",
+      interpreter: "python", // hoặc "python" tùy hệ thống
+      args: "",
+      cwd: "./mt5-agent", // thư mục chứa file mt5_connect.py
+      watch: false,
+      autorestart: true,
+      max_restarts: 10,
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
